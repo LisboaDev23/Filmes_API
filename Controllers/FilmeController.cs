d52a66db5351a649b28d17ad9c5919600d2c34a5
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2025-09-16 21:49:18 -0300

    init: inicializando projeto e aprendendo a utilizar data anottations com a criação de novas rotas na controller

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private ILogger<FilmeController> _logger;
+
+    public FilmeController(ILogger<FilmeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("/adicionar-filme")]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        _logger.LogInformation($"Filme {filme.Titulo} adicionado com sucesso.");
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O diretor do filme é obrigatório")]
+    public string Diretor { get; set; }
+    [Required(ErrorMessage = "A duração do filme é obrigatório")]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O gênero não pode ter mais de 50 caracteres")]
+    public string Genero { get; set; }
+}
